455586d8edb4e67e7e04263945af87b7a18fc19b
Yuichiro Tachibana (Tsuchiya) <t.yic.yt@gmail.com>
2021-11-17 22:41:24 +0900

    Create <ImageDataPreview />

---
+++ streamlit_fesion/frontend/src/ImageDataPreview.tsx
@@ -0,0 +1,39 @@
+import React, { useEffect, useRef } from "react"
+import { Streamlit } from "streamlit-component-lib"
+
+interface ImageDataPreviewProps {
+  imageData: ImageData
+}
+const ImageDataPreview: React.VFC<ImageDataPreviewProps> = (props) => {
+  const canvasRef = useRef<HTMLCanvasElement | null>(null)
+
+  const imageData = props.imageData
+
+  // Size adjustment
+  useEffect(() => {
+    if (canvasRef.current == null) {
+      return
+    }
+
+    const canvasElem = canvasRef.current
+    canvasElem.width = imageData.width
+    canvasElem.height = imageData.height
+
+    Streamlit.setFrameHeight()
+  }, [imageData.width, imageData.height])
+
+  // Draw canvas
+  useEffect(() => {
+    if (canvasRef.current == null) {
+      return
+    }
+
+    const canvasElem = canvasRef.current
+    const ctx = canvasElem.getContext("2d")
+    ctx?.putImageData(imageData, 0, 0)
+  }, [imageData])
+
+  return <canvas ref={canvasRef} />
+}
+
+export default ImageDataPreview
--- streamlit_fesion/frontend/src/MyComponent.tsx
+++ streamlit_fesion/frontend/src/MyComponent.tsx
@@ -1,12 +1,21 @@
 import React, { useState, useCallback } from "react"
 import { useCamera } from "./camera"
+import ImageDataPreview from "./ImageDataPreview"
 
 const MyComponent: React.VFC = () => {
   const [playing, setPlaying] = useState(false)
-  const togglePlaying = useCallback(() => setPlaying((cur) => !cur), [])
+
+  const play = useCallback(() => setPlaying(true), [])
+  const stop = useCallback(() => {
+    setPlaying(false)
+    setFrame(undefined)
+  }, [])
+
+  const [frame, setFrame] = useState<ImageData>()
 
   const onFrame = useCallback((imageData: ImageData) => {
     console.log("onFrame", imageData)
+    setFrame(imageData)
   }, [])
 
   useCamera({
@@ -17,7 +26,14 @@ const MyComponent: React.VFC = () => {
 
   return (
     <div>
-      <button onClick={togglePlaying}>{playing ? "Stop" : "Play"}</button>
+      <div>{frame && <ImageDataPreview imageData={frame} />}</div>
+      <div>
+        {playing ? (
+          <button onClick={stop}>Stop</button>
+        ) : (
+          <button onClick={play}>Play</button>
+        )}
+      </div>
     </div>
   )
 }
