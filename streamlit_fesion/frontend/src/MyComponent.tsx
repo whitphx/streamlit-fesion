97187844c4be3740314ea9a6eabd637129ce3954
Yuichiro Tachibana (Tsuchiya) <t.yic.yt@gmail.com>
2021-11-18 14:18:11 +0900

    Update to inject Python function as a image filter

--- streamlit_fesion/frontend/src/MyComponent.tsx
+++ streamlit_fesion/frontend/src/MyComponent.tsx
@@ -2,7 +2,8 @@ import React, { useState, useCallback, useEffect } from "react";
 import { useCamera } from "./camera";
 import { usePyodide } from "./PyodideProvider";
 import ImageDataPreview from "./ImageDataPreview";
-import pythonProgram from "!!raw-loader!./program.py";
+
+type ImageDataFilter = (imageData: ImageData) => Promise<ImageData>;
 
 const MyComponent: React.VFC = () => {
   const [playing, setPlaying] = useState(false);
@@ -15,17 +16,99 @@ const MyComponent: React.VFC = () => {
 
   const [frame, setFrame] = useState<ImageData>();
 
+  // TODO: Make these injectable
+  const imageFilterPyFuncDefCode = `
+  import skimage
+
+  def filter(input_image):
+      grayscale = skimage.color.rgb2gray(input_image)
+      return skimage.color.gray2rgb(grayscale)
+  `;
+  const imageFilterPyFuncName = "filter";
+  const iamgeFilterDepPackages = ["scikit-image"];
+  const imageFilterDepPackagesJson = JSON.stringify(iamgeFilterDepPackages); // Serialize for memoization
+
+  const [imageDataFilter, setImageDataFilter] =
+    useState<{ fn: ImageDataFilter }>();
+
   const pyodide = usePyodide();
-  const [pyPackageLoaded, setPyPackageLoaded] = useState(false);
   useEffect(() => {
     if (pyodide == null) {
       return;
     }
 
-    pyodide
-      .loadPackage(["numpy"]) // numpy is used in the Python program
-      .then(() => setPyPackageLoaded(true));
-  }, [pyodide]);
+    const filterDepPackages: string[] = JSON.parse(imageFilterDepPackagesJson);
+
+    (async () => {
+      await pyodide
+        .loadPackage(["numpy"]) // numpy is used in the Python program
+        .then(() => pyodide.runPythonAsync("import numpy as np"));
+
+      await pyodide.loadPackage(filterDepPackages);
+
+      // Run Python code including a function definition
+      await pyodide.runPythonAsync(imageFilterPyFuncDefCode);
+
+      const filterFn: ImageDataFilter = async (imageData) => {
+        // TODO: Run in WebWorker
+        /* eslint-disable @typescript-eslint/ban-ts-comment */
+        // @ts-ignore
+        self.fesionImageWidth = imageData.width;
+        // @ts-ignore
+        self.fesionImageHeight = imageData.height;
+        // @ts-ignore
+        self.fesionImageData = imageData.data;
+        /* eslint-enable */
+
+        await pyodide.runPythonAsync(`
+        from js import fesionImageWidth, fesionImageHeight, fesionImageData  # Import from JS globals
+
+        input_image4chan = np.asarray(fesionImageData.to_py()).reshape((fesionImageHeight, fesionImageWidth, 4)) # 4 channels (RGBA)
+        input_image = input_image4chan[:,:,:3]
+
+        output_image = ${imageFilterPyFuncName}(input_image)
+
+        if np.issubdtype(output_image.dtype, np.floating):
+            output_image = (output_image * 255).astype(np.uint8)
+
+        output_alpha = np.ones((fesionImageHeight, fesionImageWidth, 1), dtype=np.uint8) * 255
+        output_image4chan = np.concatenate((output_image, output_alpha), axis=2).copy()
+
+        output_height, output_width = output_image4chan.shape[:2]
+        `);
+
+        const outputImageProxy: PyProxy =
+          pyodide.globals.get("output_image4chan");
+        const outputImageBuffer = outputImageProxy.getBuffer("u8");
+        outputImageProxy.destroy();
+
+        const outputWidth: number = pyodide.globals.get("output_width");
+        const outputHeight: number = pyodide.globals.get("output_height");
+
+        try {
+          const newImageData = new ImageData(
+            new Uint8ClampedArray(
+              outputImageBuffer.data.buffer,
+              outputImageBuffer.data.byteOffset,
+              outputImageBuffer.data.byteLength
+            ),
+            outputWidth,
+            outputHeight
+          );
+          return newImageData;
+        } finally {
+          outputImageBuffer.release();
+        }
+      };
+
+      setImageDataFilter({ fn: filterFn });
+    })();
+  }, [
+    pyodide,
+    imageFilterPyFuncName,
+    imageFilterPyFuncDefCode,
+    imageFilterDepPackagesJson,
+  ]);
 
   const onFrame = useCallback(
     async (imageData: ImageData) => {
@@ -33,47 +116,15 @@ const MyComponent: React.VFC = () => {
         console.log("Pyodide is not loaded");
         return;
       }
-      if (!pyPackageLoaded) {
+      if (!imageDataFilter) {
         console.log("Python packages have not been loaded.");
         return;
       }
 
-      // TODO: Run in WebWorker
-      /* eslint-disable @typescript-eslint/ban-ts-comment */
-      // @ts-ignore
-      self.fesionImageWidth = imageData.width;
-      // @ts-ignore
-      self.fesionImageHeight = imageData.height;
-      // @ts-ignore
-      self.fesionImageData = imageData.data;
-      /* eslint-enable */
-
-      await pyodide.runPythonAsync(pythonProgram);
-
-      const outputImageProxy: PyProxy =
-        pyodide.globals.get("output_image4chan");
-      const outputImageBuffer = outputImageProxy.getBuffer("u8");
-      outputImageProxy.destroy();
-
-      const outputWidth: number = pyodide.globals.get("output_width");
-      const outputHeight: number = pyodide.globals.get("output_height");
-
-      try {
-        const newImageData = new ImageData(
-          new Uint8ClampedArray(
-            outputImageBuffer.data.buffer,
-            outputImageBuffer.data.byteOffset,
-            outputImageBuffer.data.byteLength
-          ),
-          outputWidth,
-          outputHeight
-        );
-        setFrame(newImageData);
-      } finally {
-        outputImageBuffer.release();
-      }
+      const outImageData = await imageDataFilter.fn(imageData);
+      setFrame(outImageData);
     },
-    [pyodide, pyPackageLoaded]
+    [pyodide, imageDataFilter]
   );
 
   useCamera({
--- streamlit_fesion/frontend/src/declarations.d.ts
+++ streamlit_fesion/frontend/src/declarations.d.ts
@@ -1,5 +1,5 @@
 interface PyBuffer {
-  data: TypedArray
+  data: TypedArray;
   release(): void;
 }
 
@@ -9,17 +9,16 @@ interface PyProxy {
   get(key: string): any;
   getBuffer(type: string): PyBuffer;
   destroy(destroyed_msg?: string): void;
-};
+}
 
 interface Pyodide {
-  globals: PyProxy
-  loadPackage(names: string | string[], messageCallback?: (log: string) => void, errorCallback?: (log: string) => void): Promise<void>;
+  globals: PyProxy;
+  loadPackage(
+    names: string | string[],
+    messageCallback?: (log: string) => void,
+    errorCallback?: (log: string) => void
+  ): Promise<void>;
   runPython(code: string, globals?: any): any;
   runPythonAsync(code: string): Promise<any>;
 }
 declare let loadPyodide: (options: any) => Promise<Pyodide>;
-
-declare module "!!raw-loader!*" {
-  const content: string;
-  export default content;
-}
