641b4570467aa3f0850dba1546a7478e9364cb20
Yuichiro Tachibana (Tsuchiya) <t.yic.yt@gmail.com>
2021-11-19 02:13:07 +0900

    Apply formatter

--- app.py
+++ app.py
@@ -1,9 +1,10 @@
 import streamlit as st
+
 from streamlit_fesion import streamlit_fesion
 
 
 def grayscale(input_image):
-    # Ref: https://scikit-image.org/docs/0.15.x/auto_examples/color_exposure/plot_rgb_to_gray.html
+    # Ref: https://scikit-image.org/docs/0.15.x/auto_examples/color_exposure/plot_rgb_to_gray.html  # noqa: E501
     import skimage
 
     grayscale = skimage.color.rgb2gray(input_image)
@@ -11,9 +12,9 @@ def grayscale(input_image):
 
 
 def sobel(input_image):
-    # Ref: https://scikit-image.org/docs/0.15.x/auto_examples/color_exposure/plot_adapt_rgb.html
-    from skimage.color.adapt_rgb import adapt_rgb, each_channel, hsv_value
+    # Ref: https://scikit-image.org/docs/0.15.x/auto_examples/color_exposure/plot_adapt_rgb.html  # noqa: E501
     from skimage import filters
+    from skimage.color.adapt_rgb import adapt_rgb, each_channel, hsv_value
     from skimage.exposure import rescale_intensity
 
     @adapt_rgb(each_channel)
@@ -28,11 +29,12 @@ def sobel(input_image):
 
 
 def face_detection(input_image):
-    # Ref: https://scikit-image.org/docs/0.15.x/auto_examples/applications/plot_face_detection.html
-    # TODO: This function is inefficient because detector is loaded in every frame. Create a cache mechanism.
+    # Ref: https://scikit-image.org/docs/0.15.x/auto_examples/applications/plot_face_detection.html  # noqa: E501
+    # TODO: This function is inefficient because detector is loaded
+    # TODO: in every frame. Create a cache mechanism.
     from skimage import data
-    from skimage.feature import Cascade
     from skimage.draw import rectangle
+    from skimage.feature import Cascade
 
     # Load the trained file from the module root.
     trained_file = data.lbp_frontal_face_cascade_filename()
@@ -40,15 +42,21 @@ def face_detection(input_image):
     # Initialize the detector cascade.
     detector = Cascade(trained_file)
 
-    detected = detector.detect_multi_scale(img=input_image,
-                                        scale_factor=1.2,
-                                        step_ratio=1,
-                                        min_size=(60, 60),
-                                        max_size=(123, 123))
+    detected = detector.detect_multi_scale(
+        img=input_image,
+        scale_factor=1.2,
+        step_ratio=1,
+        min_size=(60, 60),
+        max_size=(123, 123),
+    )
 
     for patch in detected:
         print(patch)
-        rr, cc = rectangle(start=(patch['c'], patch['r']), extent=(patch['width'], patch['height']), shape=input_image.shape[:2])
+        rr, cc = rectangle(
+            start=(patch["c"], patch["r"]),
+            extent=(patch["width"], patch["height"]),
+            shape=input_image.shape[:2],
+        )
         input_image[rr, cc, 0] = 255
 
     return input_image
