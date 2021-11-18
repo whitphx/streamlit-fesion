1dff06093a472526b079a96b8710feadd1afef9d
Yuichiro Tachibana (Tsuchiya) <t.yic.yt@gmail.com>
2021-11-18 13:21:51 +0900

    Inject Python program running on Pyodide to the frames

--- streamlit_fesion/frontend/src/MyComponent.tsx
+++ streamlit_fesion/frontend/src/MyComponent.tsx
@@ -15,25 +15,72 @@ const MyComponent: React.VFC = () => {
 
   const [frame, setFrame] = useState<ImageData>();
 
-  const onFrame = useCallback((imageData: ImageData) => {
-    console.log("onFrame", imageData);
-    setFrame(imageData);
-  }, []);
+  const pyodide = usePyodide();
+  const [pyPackageLoaded, setPyPackageLoaded] = useState(false);
+  useEffect(() => {
+    if (pyodide == null) {
+      return;
+    }
+
+    pyodide
+      .loadPackage(["numpy"]) // numpy is used in the Python program
+      .then(() => setPyPackageLoaded(true));
+  }, [pyodide]);
+
+  const onFrame = useCallback(
+    async (imageData: ImageData) => {
+      if (pyodide == null) {
+        console.log("Pyodide is not loaded");
+        return;
+      }
+      if (!pyPackageLoaded) {
+        console.log("Python packages have not been loaded.");
+        return;
+      }
+
+      // TODO: Run in WebWorker
+      /* eslint-disable @typescript-eslint/ban-ts-comment */
+      // @ts-ignore
+      self.fesionImageWidth = imageData.width;
+      // @ts-ignore
+      self.fesionImageHeight = imageData.height;
+      // @ts-ignore
+      self.fesionImageData = imageData.data;
+      /* eslint-enable */
+
+      await pyodide.runPythonAsync(pythonProgram);
+
+      const outputImageProxy: PyProxy =
+        pyodide.globals.get("output_image4chan");
+      const outputImageBuffer = outputImageProxy.getBuffer("u8");
+      outputImageProxy.destroy();
+
+      const outputWidth: number = pyodide.globals.get("output_width");
+      const outputHeight: number = pyodide.globals.get("output_height");
+
+      try {
+        const newImageData = new ImageData(
+          new Uint8ClampedArray(
+            outputImageBuffer.data.buffer,
+            outputImageBuffer.data.byteOffset,
+            outputImageBuffer.data.byteLength
+          ),
+          outputWidth,
+          outputHeight
+        );
+        setFrame(newImageData);
+      } finally {
+        outputImageBuffer.release();
+      }
+    },
+    [pyodide, pyPackageLoaded]
+  );
 
   useCamera({
     playing,
     videoConstraints: true,
     onFrame,
   });
-
-  const pyodide = usePyodide();
-  useEffect(() => {
-    if (pyodide == null) {
-      return;
-    }
-
-    console.log(pyodide.runPython(pythonProgram)); // XXX: Sample
-  }, [pyodide]);
 
   return (
     <div>
--- streamlit_fesion/frontend/src/declarations.d.ts
+++ streamlit_fesion/frontend/src/declarations.d.ts
@@ -1,6 +1,21 @@
+interface PyBuffer {
+  data: TypedArray
+  release(): void;
+}
+
+interface PyProxy {
+  length: number;
+  type: string;
+  get(key: string): any;
+  getBuffer(type: string): PyBuffer;
+  destroy(destroyed_msg?: string): void;
+};
+
 interface Pyodide {
-  runPython: (code: string, globals?: any) => any;
-  runPythonAsync: (code: string) => Promise<any>;
+  globals: PyProxy
+  loadPackage(names: string | string[], messageCallback?: (log: string) => void, errorCallback?: (log: string) => void): Promise<void>;
+  runPython(code: string, globals?: any): any;
+  runPythonAsync(code: string): Promise<any>;
 }
 declare let loadPyodide: (options: any) => Promise<Pyodide>;
 
