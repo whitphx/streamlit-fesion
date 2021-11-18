1b8617a84999bbfa5177e3fae3acee5f50befeb0
Yuichiro Tachibana (Tsuchiya) <t.yic.yt@gmail.com>
2021-11-18 14:25:53 +0900

    Fix numpy import name to be a random string

--- streamlit_fesion/frontend/src/MyComponent.tsx
+++ streamlit_fesion/frontend/src/MyComponent.tsx
@@ -5,6 +5,8 @@ import ImageDataPreview from "./ImageDataPreview";
 
 type ImageDataFilter = (imageData: ImageData) => Promise<ImageData>;
 
+const NUMPY_GLOBAL_ALIAS = "gai6sa2eM9Atiev5Shu5ohtie6phai8i"; // To avoid name conflict
+
 const MyComponent: React.VFC = () => {
   const [playing, setPlaying] = useState(false);
 
@@ -42,7 +44,9 @@ const MyComponent: React.VFC = () => {
     (async () => {
       await pyodide
         .loadPackage(["numpy"]) // numpy is used in the Python program
-        .then(() => pyodide.runPythonAsync("import numpy as np"));
+        .then(() =>
+          pyodide.runPythonAsync(`import numpy as ${NUMPY_GLOBAL_ALIAS}`)
+        );
 
       await pyodide.loadPackage(filterDepPackages);
 
@@ -63,16 +67,16 @@ const MyComponent: React.VFC = () => {
         await pyodide.runPythonAsync(`
         from js import fesionImageWidth, fesionImageHeight, fesionImageData  # Import from JS globals
 
-        input_image4chan = np.asarray(fesionImageData.to_py()).reshape((fesionImageHeight, fesionImageWidth, 4)) # 4 channels (RGBA)
+        input_image4chan = ${NUMPY_GLOBAL_ALIAS}.asarray(fesionImageData.to_py()).reshape((fesionImageHeight, fesionImageWidth, 4)) # 4 channels (RGBA)
         input_image = input_image4chan[:,:,:3]
 
         output_image = ${imageFilterPyFuncName}(input_image)
 
-        if np.issubdtype(output_image.dtype, np.floating):
-            output_image = (output_image * 255).astype(np.uint8)
+        if ${NUMPY_GLOBAL_ALIAS}.issubdtype(output_image.dtype, ${NUMPY_GLOBAL_ALIAS}.floating):
+            output_image = (output_image * 255).astype(${NUMPY_GLOBAL_ALIAS}.uint8)
 
-        output_alpha = np.full((fesionImageHeight, fesionImageWidth, 1), fill_value=255, dtype=np.uint8)
-        output_image4chan = np.concatenate((output_image, output_alpha), axis=2).copy()
+        output_alpha = ${NUMPY_GLOBAL_ALIAS}.full((fesionImageHeight, fesionImageWidth, 1), fill_value=255, dtype=${NUMPY_GLOBAL_ALIAS}.uint8)
+        output_image4chan = ${NUMPY_GLOBAL_ALIAS}.concatenate((output_image, output_alpha), axis=2).copy()
 
         output_height, output_width = output_image4chan.shape[:2]
         `);
