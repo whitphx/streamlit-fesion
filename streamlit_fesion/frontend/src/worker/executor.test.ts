5b64544995ca27b9597735d8736bd8f3eee4cf24
Yuichiro Tachibana (Tsuchiya) <t.yic.yt@gmail.com>
2024-03-12 01:12:27 +0900

    Fix the executor to use only the proxied object in JS (#19)

--- streamlit_fesion/frontend/src/worker/executor.test.ts
+++ streamlit_fesion/frontend/src/worker/executor.test.ts
@@ -10,39 +10,43 @@ describe("ImageFilterExecutor", () => {
     pyodide = await setupPyodideForTest();
   });
 
-  it("should register a Python filter function and execute it with an input image", async () => {
-    const executor = new ImageFilterExecutor(pyodide);
-    await executor.setFilterFunc(
-      "flip",
-      `
-def flip(img):
+  [true, false].forEach((isFilterFnAsync) => {
+    it(`should register a Python filter function and execute it with an input image (${
+      isFilterFnAsync ? "async" : "sync"
+    })`, async () => {
+      const executor = new ImageFilterExecutor(pyodide);
+      await executor.setFilterFunc(
+        "flip",
+        `
+${isFilterFnAsync ? "async " : ""}def flip(img):
     flipped = img[:,::-1,:]
     return flipped
-  `,
-      ["numpy"]
-    );
-    const imageData = new ImageData(
-      new Uint8ClampedArray(640 * 320 * 4),
-      640,
-      320
-    );
-    // Set white color at (0, 0)
-    imageData.data[0] = 255;
-    imageData.data[1] = 255;
-    imageData.data[2] = 255;
-    imageData.data[3] = 255;
+    `,
+        ["numpy"]
+      );
+      const imageData = new ImageData(
+        new Uint8ClampedArray(640 * 320 * 4),
+        640,
+        320
+      );
+      // Set white color at (0, 0)
+      imageData.data[0] = 255;
+      imageData.data[1] = 255;
+      imageData.data[2] = 255;
+      imageData.data[3] = 255;
 
-    const output = await executor.executeFilter(imageData);
+      const output = await executor.executeFilter(imageData);
 
-    // Except (0, 0) is black because of flip
-    expect(output.data[0]).toBe(0);
-    expect(output.data[1]).toBe(0);
-    expect(output.data[2]).toBe(0);
-    expect(output.data[3]).toBe(255);
-    // Except (639, 0) is white because of flip
-    expect(output.data[639 * 4 + 0]).toBe(255);
-    expect(output.data[639 * 4 + 1]).toBe(255);
-    expect(output.data[639 * 4 + 2]).toBe(255);
-    expect(output.data[639 * 4 + 3]).toBe(255);
+      // Except (0, 0) is black because of flip
+      expect(output.data[0]).toBe(0);
+      expect(output.data[1]).toBe(0);
+      expect(output.data[2]).toBe(0);
+      expect(output.data[3]).toBe(255);
+      // Except (639, 0) is white because of flip
+      expect(output.data[639 * 4 + 0]).toBe(255);
+      expect(output.data[639 * 4 + 1]).toBe(255);
+      expect(output.data[639 * 4 + 2]).toBe(255);
+      expect(output.data[639 * 4 + 3]).toBe(255);
+    });
   });
 });
